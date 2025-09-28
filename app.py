b09c98a2f3013d42730adf29792b3e158ea28764
MustaNG <149570897+Proffessor2008@users.noreply.github.com>
2025-09-28 18:31:18 +0300

    U

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import os
 
 from authlib.integrations.flask_client import OAuth
 from flask import Flask, request, jsonify, send_from_directory, session, redirect, url_for
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__, static_folder='.')
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 
 @app.after_request
